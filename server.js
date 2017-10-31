2f64bb463017bdc96273a686f9c9ec3e82d8b882
Tham Treerawuttichaikit <intime_@outlook.com>
2017-10-31 16:50:43 +0700

    setup package and learn about middleware

--- server.js
+++ server.js
@@ -1,11 +1,49 @@
 const PORT = process.env.PORT || 3000
 
 const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello Express!');
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`
+  
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append log to server.log')
+    }
+  })
+  next();
 });
 
-app.listen(PORT);
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  res.render('index.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome Back'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is up on port ${PORT}`);
+});
